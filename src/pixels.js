9b87b52a4f2c04f9d19ea6c9b9de781a8db9c626
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-12-30 08:11:00 +0100

    vignette

--- src/pixels.js
+++ src/pixels.js
@@ -14,6 +14,13 @@ function Pixels(rx, ry) {
         front.setTransform(ratio, 0, 0, ratio, 0, 0);
         front.imageSmoothingEnabled = false;
         front.drawImage(this.backbuffer, 0, 0);
+
+        //var vignette = front.createRadialGradient(75,50,5,90,60,100);
+        var vignette = front.createRadialGradient(el.width/8,el.height/8,0, el.width/8,el.height/8,200);
+        vignette.addColorStop(0, "transparent");
+        vignette.addColorStop(1, "rgba(0,0,0, 10)");
+        front.fillStyle = vignette;
+        front.fillRect(0, 0, el.width, el.height);
     };
 }
 
